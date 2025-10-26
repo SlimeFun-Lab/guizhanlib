94c4abd25bd16b12c8427a02190cadccdac51590
TheoDoR <teodorzabulica@gmail.com>
2025-10-26 03:24:26 +0300

    update build version for slimefun

--- guizhanlib-slimefun/build.gradle.kts
+++ guizhanlib-slimefun/build.gradle.kts
@@ -2,5 +2,5 @@ dependencies {
     api(project(":guizhanlib-common", configuration = "shadow"))
     api(project(":guizhanlib-localization", configuration = "shadow"))
     api(project(":guizhanlib-minecraft", configuration = "shadow"))
-    compileOnly("com.github.SlimeFun-Lab:Slimefun4:3ea21da")
+    compileOnly("com.github.SlimeFun-Lab:Slimefun4:6d5694e")
 }
